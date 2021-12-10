84a7ee5ab6644f9221829f0a0b775f317a9cffb9
Max <bazarsadtsogtjargal@gmail.com>
2021-12-10 13:00:26 +0900

    add functionality with javascript

---
+++ 03-picture-in-picture/script.js
@@ -0,0 +1,27 @@
+const videoElement = document.getElementById('video');
+const button = document.getElementById('button');
+
+// Prompt to select media stream and pass to video element and then play
+async function selectMediaStream() {
+	try {
+		const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+		videoElement.srcObject = mediaStream;
+		videoElement.onloadedmetadata = () => {
+			videoElement.play();
+		};
+	} catch (error) {
+		console.log('error is here');
+	}
+}
+
+button.addEventListener('click', async () => {
+	// Disable button
+	button.disabled = true;
+	// Start picture in picture
+	await videoElement.requestPictureInPicture();
+	// Reset button
+	button.disabled = false;
+});
+
+// On Load
+selectMediaStream();
