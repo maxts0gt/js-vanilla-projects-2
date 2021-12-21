30df0be1278780f13b7a6c606299a3341560635c
Max <bazarsadtsogtjargal@gmail.com>
2021-12-21 13:56:30 +0900

    add song selection feature

--- 08-music-player/script.js
+++ 08-music-player/script.js
@@ -1,8 +1,34 @@
 "use strict";
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const nextBtn = document.getElementById('next');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
-const nextBtn = document.getElementById('next');
+// Music
+const songs = [
+    {
+        name: 'jacinto-1',
+        displayName: 'Electric Chill Machine',
+        artist: 'Jacinto Design',
+    },
+    {
+        name: 'jacinto-2',
+        displayName: 'Seven Nation Army - remix',
+        artist: 'Jacinto Design',
+    },
+    {
+        name: 'jacinto-3',
+        displayName: 'Goodnight, Disco Queen',
+        artist: 'Jacinto Design',
+    },
+    {
+        name: 'metric-1',
+        displayName: 'Front Row - remix',
+        artist: 'Jacinto Design',
+    },
+];
 // Check if playing
 let isPlaying = false;
 // Play
@@ -19,5 +45,34 @@ function pauseSong() {
     playBtn.setAttribute('title', 'Play');
     music.pause();
 }
-// Play or Pause event listener
+// Update DOm
+function loadSong(song) {
+    title.textContent = song.displayName;
+    artist.textContent = song.artist;
+    music.src = `music/${song.name}.mp3`;
+    image.src = `img/${song.name}.jpg`;
+}
+// Current Song
+let songIndex = 0;
+function prevSong() {
+    songIndex--;
+    if (songIndex < 0) {
+        songIndex = songs.length - 1;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+function nextSong() {
+    songIndex++;
+    if (songIndex > songs.length - 1) {
+        songIndex = 0;
+    }
+    loadSong(songs[songIndex]);
+    playSong();
+}
+// On Load - Select First Song
+loadSong(songs[songIndex]);
+// Event Listeners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
--- 08-music-player/script.ts
+++ 08-music-player/script.ts
@@ -1,7 +1,41 @@
+const image = document.querySelector('img');
+const title = document.getElementById('title');
+const artist = document.getElementById('artist');
 const music = document.querySelector('audio');
+const nextBtn = document.getElementById('next');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
-const nextBtn = document.getElementById('next');
+
+// Object Type
+interface Song {
+	name: string;
+	displayName: string;
+	artist: string;
+}
+
+// Music
+const songs = [
+	{
+		name: 'jacinto-1',
+		displayName: 'Electric Chill Machine',
+		artist: 'Jacinto Design',
+	},
+	{
+		name: 'jacinto-2',
+		displayName: 'Seven Nation Army - remix',
+		artist: 'Jacinto Design',
+	},
+	{
+		name: 'jacinto-3',
+		displayName: 'Goodnight, Disco Queen',
+		artist: 'Jacinto Design',
+	},
+	{
+		name: 'metric-1',
+		displayName: 'Front Row - remix',
+		artist: 'Jacinto Design',
+	},
+];
 
 // Check if playing
 let isPlaying = false;
@@ -22,5 +56,39 @@ function pauseSong() {
 	music.pause();
 }
 
-// Play or Pause event listener
+// Update DOm
+function loadSong(song: Song) {
+	title.textContent = song.displayName;
+	artist.textContent = song.artist;
+	music.src = `music/${song.name}.mp3`;
+	image.src = `img/${song.name}.jpg`;
+}
+
+// Current Song
+let songIndex = 0;
+
+function prevSong() {
+	songIndex--;
+	if (songIndex < 0) {
+		songIndex = songs.length - 1;
+	}
+	loadSong(songs[songIndex]);
+	playSong();
+}
+
+function nextSong() {
+	songIndex++;
+	if (songIndex > songs.length - 1) {
+		songIndex = 0;
+	}
+	loadSong(songs[songIndex]);
+	playSong();
+}
+
+// On Load - Select First Song
+loadSong(songs[songIndex]);
+
+// Event Listeners
+prevBtn.addEventListener('click', prevSong);
+nextBtn.addEventListener('click', nextSong);
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
