f510cbd0ad8419c6938d686bb0a5e090a4c3b93d
Max <bazarsadtsogtjargal@gmail.com>
2021-12-21 13:15:52 +0900

    add play/pause feature

--- 08-music-player/script.js
+++ 08-music-player/script.js
@@ -1,26 +1,23 @@
+"use strict";
 const music = document.querySelector('audio');
 const prevBtn = document.getElementById('prev');
 const playBtn = document.getElementById('play');
 const nextBtn = document.getElementById('next');
-
 // Check if playing
 let isPlaying = false;
-
 // Play
 function playSong() {
-	isPlaying = true;
-	playBtn.classList.replace('fa-play', 'fa-pause');
-	playBtn.setAttribute('title', 'Pause');
-	music.play();
+    isPlaying = true;
+    playBtn.classList.replace('fa-play', 'fa-pause');
+    playBtn.setAttribute('title', 'Pause');
+    music.play();
 }
-
 // Pause
 function pauseSong() {
-	isPlaying = false;
-	playBtn.classList.replace('fa-pause', 'fa-play');
-	playBtn.setAttribute('title', 'Play');
-	music.pause();
+    isPlaying = false;
+    playBtn.classList.replace('fa-pause', 'fa-play');
+    playBtn.setAttribute('title', 'Play');
+    music.pause();
 }
-
 // Play or Pause event listener
 playBtn.addEventListener('click', () => (isPlaying ? pauseSong() : playSong()));
