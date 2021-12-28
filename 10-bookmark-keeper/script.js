df4153b2924da22eb6b5823cb49914386e74303e
Max <bazarsadtsogtjargal@gmail.com>
2021-12-28 15:10:48 +0900

    add form validation

--- 10-bookmark-keeper/script.js
+++ 10-bookmark-keeper/script.js
@@ -6,6 +6,8 @@ const websiteNameEl = document.getElementById('website-name');
 const websiteUrlEl = document.getElementById('website-url');
 const bookmarksContainer = document.getElementById('bookmarks-container');
 
+let bookmarks = [];
+
 // Show Modal, focus on input
 
 console.log(modalShow);
@@ -23,3 +25,40 @@ modalClose.addEventListener('click', () =>
 window.addEventListener('click', (e) =>
 	e.target === modal ? modal.classList.remove('show-modal') : false
 );
+
+// validate form
+function validate(nameValue, urlValue) {
+	const expression =
+		/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g;
+	const regex = new RegExp(expression);
+
+	if (!nameValue || !urlValue) {
+		alert('Please submit values for both fields.');
+		return false;
+	}
+
+	if (urlValue.match(regex)) {
+		alert('match');
+	}
+	if (!urlValue.match(regex)) {
+		alert('Please provide a valid web address');
+	}
+	// valid
+	return true;
+}
+
+// Handledata from Bookmark
+function storeBookmark(e) {
+	e.preventDefault();
+	const nameValue = websiteNameEl.value;
+	let urlValue = websiteUrlEl.value;
+	if (!urlValue.includes('http://', 'https://')) {
+		urlValue = `https://${urlValue}`;
+	}
+	if (!validate(nameValue, urlValue)) {
+		return false;
+	}
+}
+
+// Event Listener
+bookmarkForm.addEventListener('submit', storeBookmark);
